341fc388b23cb9e436bbb035dc18fb122c7d0de4
saltyfisher <983415847@qq.com>
2025-02-20 16:18:25 +0800

    1

--- core/MFCAugment.py
+++ core/MFCAugment.py
@@ -11,7 +11,7 @@ import joblib
 # import geatpy as ea
 
 from tqdm import tqdm
-from torchvision.transforms import transforms
+from torchvision.transforms import transforms, ToTensor
 from torch.utils.data import Dataset, DataLoader
 from itertools import chain
 from PIL import Image
@@ -27,22 +27,29 @@ from EA.MFPSO import MFPSO
 from dataclasses import dataclass
 
 class SingleTask(object):
-    def __init__(self,dim,Lb,Ub,encode,fnc):
-        self.dim = dim
-        self.Lb = Lb
-        self.Ub = Ub
+    def __init__(self,dim:int,Lb:list[int],Ub:list[int],encode:list[int],fnc):
+        self.dims = dim
+        self.Lb = np.array(Lb)
+        self.Ub = np.array(Ub)
         self.encode = encode
         self.evalfnc = fnc
 
-def evalFunc(policy, group_id, params):
-    feat_extractor = params['feat_extracor']
+    def evaluate(self, x, params):
+        params.update({'Lb':self.Lb,'Ub':self.Ub})
+        return self.evalfnc(x, params)
+def evalFunc(policy, params):
+    feat_extractor = params['feat_extractor']
     data_list = params['data_list']
     groups = params['groups']
     scaler = params['scaler']
     gm = params['gm']
     w = params['w']
+    group_id = params['task_id']
+    Lb = params['Lb']
+    Ub = params['Ub']
+    policy = np.floor(policy*(Ub-Lb)+Lb)
     aug = MyAugment(policy)
-    aug_imgs = [aug(data_list[i]) for i in list(np.argwhere(groups == group_id))]
+    aug_imgs = [aug(ToTensor(data_list[i])) for i in list(np.argwhere(groups == group_id).flatten())]
     aug_feat = feat_extractor(aug_imgs)
     aug_feat = scaler.transform(aug_feat)
     aug_mean = np.mean(aug_feat, axis=0)
@@ -54,7 +61,7 @@ def evalFunc(policy, group_id, params):
 def MFCAugment(dataset, gaussian_component=3,n_op=5,parameter_max=31):
     total_op_num = len(augmentation_space())
     ###提取特征###
-    data_list = dataset.all_files
+    data_list = dataset.get_all_files()
     data_list = [np.array(d) for d in data_list]
     feat_extractor = FeatureExtractor()
     feat_list = feat_extractor(data_list)
@@ -66,10 +73,10 @@ def MFCAugment(dataset, gaussian_component=3,n_op=5,parameter_max=31):
     w = 1/(w/np.linalg.norm(w, axis=1) + np.eye(w.shape[0]))
     ###定义子任务###
     Lb = [0]*(n_op*3)
-    Ub = ([total_op_num-1]*(n_op)).extend([parameter_max]*n_op).extend([parameter_max]*n_op)
-    tasks = [SingleTask(n_op*3,Lb,Ub,[0]*(n_op*3)) for _ in range(np.max(groups),evalFunc)]
+    Ub = ([total_op_num-1]*(n_op)) + ([parameter_max-1]*n_op) + ([parameter_max-1]*n_op)
+    tasks = [SingleTask(n_op*3,Lb,Ub,[0]*(n_op*3), evalFunc) for _ in range(np.max(groups)+1)]
     params = {'feat_extractor':feat_extractor, 'data_list':data_list, 'groups':groups, 'scaler':scaler, 'gm':gm, 'w':w}
-    MFPSO(tasks, 100, 50, 0.3, 0, 20, params)
+    MFPSO(tasks, 100, 50, 0.3, 20, params)
     
 
 
